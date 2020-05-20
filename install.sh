390d45ade08599296eedabf8f33db5c7bcecb6ce
dyao <djy@sourcepad.com>
2020-05-20 16:25:41 +0800

    adjustments to overall structure of alias

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
-touch .alias
-rm .alias
+#!/bin/bash
+
+# add array value based on alias/<filename>.txt
+to_installs=(capistrano rails git)
+> alias.txt
+for i in ${to_installs[*]}
+do
+  echo """
+================
+$i
+================
+  """ >> alias.txt
+  cat alias/"$i".txt >> alias.txt
+done
+
 cp alias.txt .alias
+
 mv .alias $HOME
 cd $HOME
 grep -qxF 'source "$HOME/.alias"' .profile || echo 'source "$HOME/.alias"' >> .profile
